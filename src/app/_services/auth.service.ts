5e6c88c7b80df26fce3acc756ead7e80cb311e14
cronus712 <rayengrid@gmail.com>
2024-02-22 18:51:09 +0100

    auth service

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+ import { HttpClient, HttpHeaders } from '@angular/common/http';
+ import { Movie } from '../movie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+ private baseUrl = 'http://localhost:3000/movies'
+
+  httpOptions = {
+  headers: new HttpHeaders ({'Content-Type' : 'application/json'})
+ };
+
+  constructor(private http : HttpClient) { }
+
+  login(username: string, password: string) : Observable<any> {
+    return this.http.post(this.baseUrl + 'signIn', {username, password},this.httpOptions);
+  }
+
+  register(username : string, password : string, email : string) {
+    return this.http.post(this.baseUrl+'signUp', {username, password, email}, this.httpOptions);
+  }
+
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './components/auth/auth.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BoardAdminComponent } from './components/board-admin/board-admin.component';
+import { BoardUserComponent } from './components/board-user/board-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardUserComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
