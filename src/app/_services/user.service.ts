be7d0507383c7695afeb1285af94821763e8b54c
cronus712 <rayengrid@gmail.com>
2024-02-23 16:32:40 +0100

    auth interceptor

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:3000/movies'
+
+  constructor(private http:HttpClient) { }
+
+ getPublicContent():Observable<any> {
+  return this.http.get(this.baseUrl + 'all', {'responseType': 'text'});
+
+}
+
+getUserBoard(): Observable<any> {
+  return this.http.get(this.baseUrl + 'user', {'responseType': 'text'});
+}
+
+getAdminBoard(): Observable<any> {
+  return this.http.get(this.baseUrl + 'admin', {'responseType' : 'text'});
+}
+
 }
