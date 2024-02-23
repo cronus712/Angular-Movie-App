60424a5b18e6dccb4a4a58f8d266348b4304f8a8
cronus712 <rayengrid@gmail.com>
2024-02-23 16:09:01 +0100

    token storage service

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
  private baseUrl = 'http://localhost:3000/movies'
--- src/app/_services/token-storage.service.ts
+++ src/app/_services/token-storage.service.ts
@@ -4,6 +4,29 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TokenStorageService {
+  TOKEN_KEY = 'auth-token';
+  USER_KEY = 'auth-user';
 
   constructor() { }
+
+  signOut(): void {
+    window.sessionStorage.clear();
+  }
+
+  public saveToken(token: string): void {
+    window.sessionStorage.removeItem(this.TOKEN_KEY);
+    window.sessionStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  public getToken(): string | null {
+    return window.sessionStorage.getItem(this.TOKEN_KEY);
+  }
+
+  public getUser(): any {
+    const user = window.sessionStorage.getItem(this.USER_KEY);
+    if (user) {
+      return JSON.parse(user);
+    }
+    return {};
+  }
 }
